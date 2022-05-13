76f93ef3ca82ea93c756330813017e033fb4acf4
Adrian Florescu <florescuadyz@yahoo.com>
2022-05-13 20:47:59 +0300

    Added cache3

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,21 +1,32 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import {renderToNodeStream } from "react-dom/server";
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+
+  let stream = await renderToNodeStream(<RemixServer context={remixContext} url={request.url} />);
+
+    // and transform it to a Buffer to send in the Response
+    let body: Buffer = await new Promise((resolve, reject) => {
+      let buffers: Uint8Array[] = [];
+      stream.on("data", (data) => {
+        buffers.push(data);
+      });
+      stream.on("end", () => {
+        resolve(Buffer.concat(buffers));
+      });
+      stream.on("error", reject);
+    });
 
   responseHeaders.set("Content-Type", "text/html");
   responseHeaders.set("cache-control", "public, max-age=30, s-maxage=86400")
 
-  return new Response("<!DOCTYPE html>" + markup, {
+  return new Response(body, {
     status: responseStatusCode,
     headers: responseHeaders,
   });
